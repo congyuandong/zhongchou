88a0af83f1da11c116f78413ee257d0b40e8e709
congyuandong <congyuandong@gmail.com>
2014-07-17 23:36:02 +0800

    修改筛选

--- app/Lib/modules/transferModule.class.php
+++ app/Lib/modules/transferModule.class.php
@@ -2,13 +2,53 @@
 require APP_ROOT_PATH.'app/Lib/page.php';
 class transferModule extends BaseModule{
 	public function index(){
+		session_start();
+		$com_type = strim($_REQUEST['com_type']);
+		$last_day = strim($_REQUEST['last_day']);
+		$scale = strim($_REQUEST['scale']);
+		$deal_status = strim($_REQUEST['deal_status']);
+
+		if($com_type == "" && $last_day == "" && $scale == "" && $deal_status == ""){
+			$_SESSION['CACHE_T_COM_TYPE'] = "";$_SESSION['CACHE_T_LAST_DAY'] = "1";
+			$_SESSION['CACHE_T_SCALE'] = "1";$_SESSION['CACHE_T_DEAL_STATUS'] = "";
+		}
+		if($com_type != "")
+			$_SESSION['CACHE_T_COM_TYPE'] = $com_type;
+		if($last_day != "")
+			$_SESSION['CACHE_T_LAST_DAY'] = $last_day;
+		if($scale != "")
+			$_SESSION['CACHE_T_SCALE'] = $scale;
+		if($deal_status != "")
+			$_SESSION['CACHE_T_DEAL_STATUS'] = $deal_status;
+
+		$t_g_com_type = $_SESSION['CACHE_T_COM_TYPE'];
+		$GLOBALS['tmpl']->assign("t_g_com_type",$t_g_com_type);
+		$t_g_last_day = $_SESSION['CACHE_T_LAST_DAY'];
+		$GLOBALS['tmpl']->assign("t_g_last_day",$t_g_last_day);
+		$t_g_scale = $_SESSION['CACHE_T_SCALE'];
+		$GLOBALS['tmpl']->assign("t_g_scale",$t_g_scale);
+		$t_g_deal_status = $_SESSION['CACHE_T_DEAL_STATUS'];
+		$GLOBALS['tmpl']->assign("t_g_deal_status",$t_g_deal_status);
+
+		$condition = " is_delete = 0 ";
+		if($t_g_com_type!="" && $t_g_com_type!="all")
+			$condition .= " and com_type = '".$t_g_com_type."'";
+		if($t_g_deal_status!="" && $t_g_deal_status!="all")
+			$condition .= " and deal_status = '".$t_g_deal_status."'";
+
+		$com_type_list = $GLOBALS['db']->getAll("select distinct com_type from ".DB_PREFIX."transfer order by sort asc");
+		$GLOBALS['tmpl']->assign("com_type_list",$com_type_list);
+		$deal_status_list = $GLOBALS['db']->getAll("select distinct deal_status from ".DB_PREFIX."transfer order by sort asc");
+		$GLOBALS['tmpl']->assign("deal_status_list",$deal_status_list);
+
+
 		$page_size = DEAL_PAGE_SIZE;
 		$page = intval($_REQUEST['p']);
 		if($page==0)$page = 1;		
 		$limit = ($page-1)*$page_size.",".$page_size;
 
 
-		$condition = " is_delete = 0 "; 
+		 
 		
 		$deal_count = $GLOBALS['db']->getOne("select count(*) from ".DB_PREFIX."transfer where ".$condition);
 		$page = new Page($deal_count,$page_size);   //初始化分页对象 		
